95401f0111849f058139673e3a99020a22ec1243
zna.kshi <zinabalingan@gmail.com>
2025-03-06 16:15:15 +0800

    PHP exrcise 2 (copy pasted from programiz to vs code for pushing

--- PHP Exercises/phpExercise.php
+++ PHP Exercises/phpExercise.php
@@ -5,4 +5,21 @@
     echo "Difference: " . $a - $b . "\n";
     echo "Product: " . $a * $b . "\n";
     echo "quotient: " . $a / $b . "\n";
+
+    $sum = $a + $b;
+    
+    if ($sum% 2 == 0){
+        echo "Number is even";
+    }   else{
+        echo "NUmer is odd";
+    }
+    
+    if ($sum >= 1){
+        echo "\nNumber is possitive";
+    }   elseif ($sum < 0){
+        echo "\nnumber is negative";
+    }   else{
+        echo "\nnumber is zero";
+    }
 ?>
+
